b48cc497e3c32f5ff015b3ee1ebed885b3cff314
ietsuneyushi <b1022016@fun.ac.jp>
2024-07-08 23:51:34 +0900

    1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,9 @@
-export default function Home() {
+"use client";
+export default function About() {
   return (
     <div>
       <h2 className="font-bold" style={{ fontSize: `20px`, margin: `3%` }}>
-        ホーム画面
+        about画面
       </h2>
     </div>
   );
--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,8 +1,9 @@
-export default function Home() {
+"use client";
+export default function Content() {
   return (
     <div>
       <h2 className="font-bold" style={{ fontSize: `20px`, margin: `3%` }}>
-        ホーム画面
+        content画面
       </h2>
     </div>
   );
