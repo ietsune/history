a005ee8a5c305e4dac3be327f1ca1ed8a662e204
ietsuneyushi <b1022016@fun.ac.jp>
2024-07-10 23:11:25 +0900

    Initial commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,9 @@ export default function About() {
       <h2 className="font-bold" style={{ fontSize: `20px`, margin: `3%` }}>
         about画面
       </h2>
+      <div>
+        私は公立はこだて未来大学<p>せっかく</p>のものです
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,53 @@
-export default function Home() {
+import Linkify from "linkify-react";
+import { LinkifyProps } from "linkify-react";
+
+interface RenderProps {
+  attributes: React.HTMLProps<HTMLAnchorElement>;
+}
+
+const Home = () => {
+  // 共通のレンダリング関数
+  const renderLink = (content: string, className: string) => {
+    const LinkComponent = ({ attributes }: RenderProps) => (
+      <a {...attributes} style={{ textDecoration: "none" }}>
+        <div className={className}>
+          <span>{content}</span>
+        </div>
+      </a>
+    );
+    LinkComponent.displayName = `LinkComponent(${content})`;
+    return LinkComponent;
+  };
+
+  // オプションの作成
+  const createOptions = (
+    content: string,
+    className: string
+  ): LinkifyProps["options"] => ({
+    defaultProtocol: "https",
+    target: "_blank",
+    render: renderLink(content, className),
+  });
+
+  const xoptions = createOptions("x", "link-container");
+  const instoptions = createOptions("Instagram", "link-container-inst");
+
+  const x = "https://x.com/?lang=ja";
+  const inst = "https://www.instagram.com/";
+
   return (
     <div>
-      <h2 className="font-bold" style={{ fontSize: `20px`, margin: `3%` }}>
-        ホーム画面
+      <h2 className="font-bold" style={{ fontSize: "20px", margin: "3%" }}>
+        私のアカウント
       </h2>
+      <Linkify options={xoptions}>{x}</Linkify>
+      <p style={{ margin: "30px" }}></p>
+      <Linkify options={instoptions}>{inst}</Linkify>
+      <p style={{ margin: "1000px" }}></p>
     </div>
   );
-}
+};
+
+Home.displayName = "Home";
+
+export default Home;
